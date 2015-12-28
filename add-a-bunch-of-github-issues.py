487dfaaeeaa87b22b7e725584b836bcb31509286
Evan Hahn <me@evanhahn.com>
2015-12-28 08:24:18 -0800

    Use consistent quotes

--- add-a-bunch-of-github-issues.py
+++ add-a-bunch-of-github-issues.py
@@ -11,7 +11,7 @@ reponame = raw_input("what's the repo called? ")
 
 filename = raw_input("what file has all of the issues? ")
 
-with open(filename, 'r') as file:
+with open(filename, "r") as file:
     for unstripped_line in file:
         line = unstripped_line.strip()
 
@@ -19,11 +19,11 @@ with open(filename, 'r') as file:
             gh = login(token=gh_token)
             repo = gh.repository(owner, reponame)
 
-            print('adding ' + line),
+            print("adding " + line),
             repo.create_issue(line)
 
-            print('- added. sleeping for'),
+            print("- added. sleeping for"),
             sleeping_time = randint(10, 45)
             print(sleeping_time),
-            print('seconds')
+            print("seconds")
             sleep(sleeping_time)
